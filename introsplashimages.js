c6e313e0592a7c6e7d3a72348bd44f068f3e0a35
Zanele Munyikwa <ztm3@duke.edu>
2014-03-30 12:20:50 -0400

    added intro splash pages

---
+++ introsplashimages.js
@@ -0,0 +1,31 @@
+var current = 0;
+var splashimages;
+window.onload = function() {
+splashimages = ["http://i1170.photobucket.com/albums/r537/natasial1/intro1_zpsf8a70abf.jpg", "http://i1170.photobucket.com/albums/r537/natasial1/intro2_zps22b765f1.jpg", "http://i1170.photobucket.com/albums/r537/natasial1/intro3_zps92ac072e.jpg"];
+
+   var project_page = document.getElementById('splashimage');
+   project_page.innerHTML= "<img src=" + splashimages[current]+ "> </img>";
+                                      
+  current = current +1;
+  
+};
+
+
+$( "#splashimage" ).click(function() {
+alert("click is recognized");
+  var project_page = document.getElementById('splashimage');
+   project_page.innerHTML="<img src=" + splashimages[current]+ "> </img>";
+                                             current = current + 1; 
+if (current == 3) {
+  window.location.href = 'filters.html';
+}
+});
+
+   
+               
+               
+
+
+
+
+
--- projectdisplay.js
+++ projectdisplay.js
@@ -1,26 +1,91 @@
+var projectnames;
+var projectimages; 
+var projectcosts;
+var projectdescriptions;
+var swipes = 0;
+var current = 0; 
+var max; 
 window.onload = function() {
+  loadProjectNames();
+  loadProjectImages();
+  loadDescriptions();
+  loadProjectCosts();
   
   showImage();
   showTitle();
   showDescription();
+  showMoney();
+  
 };
+
+function loadProjectNames() {
+  projectnames = ["waffle", "waffle2", "waffle3"];
+}
+
+function loadProjectImages() {
+  projectimages = ["http://lorempixel.com/400/200/sports/", "http://lorempixel.com/output/food-q-c-640-480-6.jpg", "http://lorempixel.com/output/fashion-q-c-640-480-7.jpg"];
+}
+
+function loadDescriptions() {
+  projectdescriptions = ["description1", "description2", "description3"];
+  
+}
+
+function loadProjectCosts() {
+  projectcosts = ["5", "6", "7"];
+}
+
 
 function showImage(){
  var project_page = document.getElementById('shownproject');
-  project_page.querySelector('p.image').innerHTML = "<img src= 'kittens.png'> </img>";
+  project_page.querySelector('p.image').innerHTML = "<img src=" + projectimages[current]+ "> </img>";
  
   
 }
 
 function showTitle() {
   var project_page = document.getElementById('shownproject');
-   project_page.querySelector('p.title').innerHTML= "Independent Animal Rescue Inc.";
+   project_page.querySelector('p.title').innerHTML= projectnames[current];
 }
+
+
+function showTitle2() {
+  var project_page = document.getElementById('shownproject');
+   project_page.querySelector('p.title').innerHTML= "Woot";
+}
+
 
 function showDescription(){
     var project_page = document.getElementById('shownproject');
-   project_page.querySelector('p.description').innerHTML= "Provide the full cost of veterinary care for a litter of kittens.";
+   project_page.querySelector('p.description').innerHTML= projectdescriptions[current];
 }
+
+
+function showMoney(){
+    var project_page = document.getElementById('shownproject');
+   project_page.querySelector('p.dollars').innerHTML= projectcosts[current];
+}
+
+$( "#shownproject" ).dblclick(function() {
+  alert( "Handler for .dblclick() called." );
+  current = current +1; 
+  showImage();
+  showTitle();
+  showDescription();
+  showMoney();
+});
+
+$("body").bind("swipeleft", function(e) {
+              showTitle2();
+               });
+               
+$("body").bind("swiperight", function(e) {
+  showTitle2();
+  
+});
+               
+               
+               
 
 
 
